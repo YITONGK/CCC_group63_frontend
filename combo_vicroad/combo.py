e52280aee1dbcff50be0a15543f5d32eb83953fb
ZWA138 <denniswang823@gmail.com>
2024-05-01 22:33:25 +1000

    combo finish

--- combo_vicroad/combo.py
+++ combo_vicroad/combo.py
@@ -1,45 +1,81 @@
-import os
-import io
-import csv
-import json
 import requests
-from flask import current_app, request
-from elasticsearch8 import Elasticsearch
+import json
+from elasticsearch8 import Elasticsearch, helpers
+import time
 
-def extract_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Use csv.reader to parse the CSV data from the response
-        reader = csv.DictReader(io.StringIO(response.text))
-
-        # Convert the CSV data to JSON by reading it into a list of dicts
-        json_data = json.dumps(list(reader))  # Use indent for pretty-printing
-
-        # Return or print the JSON data
-        return json_data
-    else:
-        print("Failed to fetch data, status code:", response.status_code)
 
 def main():
-    url = 'https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Road_Safety/ACCIDENT.csv'
-    # Extract data and print
-    data = extract_data(url)
+    # return "check connection"
 
-    client = Elasticsearch (
-        'https://elasticsearch-master.elastic.svc.cluster.local:9200',
+    url = "https://discover.data.vic.gov.au/api/3/action/datastore_search"
+    resource_id = "d48aa391-9f43-4c67-bd90-81192ff2e732"
+    limit = 5000
+    offset = 140649
+    # end_point = 155649  # No output
+    # offset = 155665
+    end_point = 167300
+    client = Elasticsearch(
+        "https://elasticsearch-master.elastic.svc.cluster.local:9200",
         verify_certs=False,
-        basic_auth=('elastic', 'elastic')
+        basic_auth=("elastic", "elastic"),
     )
-    json_data = json.loads(data)
-    for obs in json_data:
-        print("here")
-        res = client.index(
-            #index = index_name,
-            index='accidents',
-            id=obs.get("ACCIDENT_NO"),
-            body=obs
-        )
-        current_app.logger.info(f'Indexed observation {obs["ACCIDENT_NO"]}')
+    count = 0
+    actions = []
+    # session = requests.Session()
+    while offset <= end_point:
+        params = {"resource_id": resource_id, "limit": limit, "offset": offset}
 
-    return 'ok'
+        try:
+            response = requests.get(url, params=params, verify=False)
+            if response.status_code == 200:
+                data = response.json()
+                records = data["result"]["records"]
 
+                for obs in records:
+                    action = {
+                        "_index": "accidents",
+                        "_id": obs["_id"],
+                        "_op_type": "index",  # Use 'index' to create or replace a document
+                        "_source": {
+                            key: obs[key]
+                            for key in obs
+                            if key
+                               not in [
+                                   "_id",
+                                   "RMA",
+                                   "DAY_WEEK_DESC",
+                                   "DCA_CODE",
+                                   "DCA_DESC",
+                                   "LIGHT_CONDITION",
+                                   "POLICE_ATTEND",
+                               ]
+                        },
+                    }
+                    actions.append(action)
+                    count += 1
+                    if (
+                            len(actions) == 500
+                    ):  # When accumulated 500 actions, execute bulk operation
+                        helpers.bulk(client, actions)
+                        actions = []  # Clear actions after sending
+
+                if len(records) < limit:
+                    break  # Break the loop if last batch
+                offset += limit
+            else:
+                print("Failed to fetch data: " + str(response))
+                break  # Break the loop on failure
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break  # Break the loop on exception
+
+    if actions:  # Ensure all remaining actions are sent
+        helpers.bulk(client, actions)
+
+    return json.dumps(
+        {"status": 200, "message": "Successfully inserted records: " + str(count)}
+    )
+
+
+print(main())
--- extractdata_vicroad/extractdata.py
+++ extractdata_vicroad/extractdata.py
@@ -1,76 +1,3 @@
-# import os
-# import io
-# import requests
-# import pandas as pd
-#
-# def extract_data(url):
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         # Read CSV data using pandas
-#         data = pd.read_csv(io.StringIO(response.text))
-#         # Convert the DataFrame to JSON format
-#         json_data = data.to_json(orient='records')
-#         # Print the converted JSON data
-#         return json_data
-#     else:
-#         print("Failed to fetch data, status code:", response.status_code)
-#
-# def main():
-#     # Get URL from environment variable
-#     #url = os.getenv('URL1')
-#     url = 'https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Road_Safety/ACCIDENT.csv'
-#     # Extract data
-#     return extract_data(url)
-#
-import os
-import io
-import csv
-import json
-import requests
-
-# def extract_data(url):
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         # Use csv.reader to parse the CSV data from the response
-#         reader = csv.DictReader(io.StringIO(response.text))
-#
-#         # Convert the CSV data to JSON by reading it into a list of dicts
-#         json_data = json.dumps(list(reader), indent=4)  # Use indent for pretty-printing
-#
-#         # Return or print the JSON data
-#         return json_data
-#     else:
-#         print("Failed to fetch data, status code:", response.status_code)
-#
-# def main():
-#     # Get URL from environment variable
-#     url = 'https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Road_Safety/ATMOSPHERIC_COND.csv'
-#     # Extract data and print
-#     return extract_data(url)
-#
-# # if __name__ == "__main__":
-# #     main()
-#import os
-#import io
-#import csv
-# import json
-# import requests
-#
-#
-# def main():
-#     url = 'https://discover.data.vic.gov.au/api/3/action/datastore_search'
-#     params = {
-#         'resource_id': 'd48aa391-9f43-4c67-bd90-81192ff2e732',
-#     }
-#     response = requests.get(url, params=params)
-#     if response.status_code == 200:
-#         data = response.json()
-#         records = data['result']['records']
-#         json_data = json.dumps(records)
-#         return json_data
-#     else:
-#         print("Failed to fetch data, status code:", response.status_code)
-
 import requests
 import json
 
@@ -79,10 +6,10 @@ def main():
     resource_id = 'd48aa391-9f43-4c67-bd90-81192ff2e732'
     limit = 100
     offset = 155665
-    end_point = 167110
+    end_point = 167300
     all_records = []
 
-    while offset <= 155766:
+    while offset <= end_point:
         params = {
             'resource_id': resource_id,
             'limit': limit,
@@ -91,12 +18,8 @@ def main():
         response = requests.get(url, params=params)
         if response.status_code == 200:
             data = response.json()
-            print(data)
             records = data['result']['records']
-            for obs in records:
-                id = obs["_id"]
-                print(id)
-
+            print(records)
             all_records.extend(records)
             if len(records) < limit:
                 break
@@ -107,4 +30,3 @@ def main():
 
     json_data = json.dumps(all_records)
     return json_data
-
